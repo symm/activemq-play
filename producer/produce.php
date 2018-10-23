ef0da888b22cf96d1ca09c8a24b5dd6930794485
Gareth Jones <me@gazj.co.uk>
2018-10-23 13:01:49 +0100

    sync

--- producer/produce.php
+++ producer/produce.php
@@ -3,17 +3,22 @@
 require_once __DIR__ . '/vendor/autoload.php';
 
 use Stomp\Client;
+use Symm\Config;
 
 $stomp = new Client('tcp://localhost:61613');
 $stomp->getConnection();
 $stomp->setClientId('CHICKEN-PRODUCER');
 
+
 while (true) {
-    $stomp->send(\Symm\Config::QUEUE_NAME, \json_encode([
-        'chicken' => '🐔',
-        'unicorn' => '🦄',
-    ]), [], null);
+    $message = \json_encode([
+            'chicken' => '🐔',
+            'unicorn' => '🦄',
+    ]);
+    $synchronous = true;
+    $header = [];
+
+    $stomp->send(Config::QUEUE_NAME, $message, $header, $synchronous);
     print '.';
-    sleep(0.5);
 }
 
